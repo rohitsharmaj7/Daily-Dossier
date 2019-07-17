e5f0045cb572c025906de9486630e5d918a8ad1d
Rohit Sharma <rohitsharmaj7@gmail.com>
2019-07-17 09:53:10 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-    module.exports={ mongoURI:'mongodb://rohitsharmaj7:ruvaina18@ds349455.mlab.com:49455/diary'}
+    module.exports={ mongoURI:'mongodb://rohitsharmaj7:rohitsharmaj7@ds349455.mlab.com:49455/diary'}
 }
 else{
     module.exports = {mongoURI:'mongodb://localhost/diary'}
